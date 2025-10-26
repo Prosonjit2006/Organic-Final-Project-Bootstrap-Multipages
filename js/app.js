232d5edc51fae05bd92fc557e12c2bea5f62f257
Prosonjit <prosonjitdatta2006@gmail.com>
2025-10-26 21:08:33 +0530

    removed the app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-$(function () {
+// $(function () {
 
 
 
 
-  //  sticky position on the navbar
+//   //  sticky position on the navbar
 
-  let stickyNavTop = $(".navbar").offset().top;
+//   let stickyNavTop = $(".navbar").offset().top;
 
-  function stickyNav() {
-    let scrollTop = $(window).scrollTop();
+//   function stickyNav() {
+//     let scrollTop = $(window).scrollTop();
 
-    if (scrollTop > stickyNavTop) {
-      $(".navbar").addClass("sticky");
-      $(".banner").css('margin-top', '78px');
-    } else {
-      $(".navbar").removeClass("sticky");
-      $(".banner").css('margin-top', '0px');
+//     if (scrollTop > stickyNavTop) {
+//       $(".navbar").addClass("sticky");
+//       $(".banner").css('margin-top', '78px');
+//     } else {
+//       $(".navbar").removeClass("sticky");
+//       $(".banner").css('margin-top', '0px');
 
-    }
-  }
+//     }
+//   }
 
-  stickyNav();
-  $(window).scroll(function () {
-    stickyNav();
-  });
+//   stickyNav();
+//   $(window).scroll(function () {
+//     stickyNav();
+//   });
 
-});
+// });
 
 
 
